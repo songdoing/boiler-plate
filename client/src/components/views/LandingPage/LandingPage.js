38cafbb72e803a5629d16c56f125b8be0655fc65
wonjinsong <msmk22239@gmail.com>
2020-05-31 23:56:15 -0400

    adds login page form

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
+
 function LandingPage() {
+
+    
     return (
-        <div>
-            LandingPage
+        <div style={{display :'flex', justifyContent : 'center', alignItems : 'center',
+            width : '100%', height : '100vh' 
+        }}>
+           <h2>start page</h2>
         </div>
     )
 }
